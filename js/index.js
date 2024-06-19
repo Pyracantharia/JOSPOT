b942d1df6f835ae344164e71bdfbbbd87bdba48b
Jonathan Assi <assijonathan2@gmail.com>
2024-06-19 20:28:02 +0200

    ajout d'une page d'erreur

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
-const burgerMenuMobile = document.getElementById("burger-menu-mobile");
-const stripesMobile = document.querySelectorAll("#event-toggler-mobile .stripe");
-const eventsSection = document.querySelector("#events-section");
-burgerMenuMobile.addEventListener("click", () => {
-    stripesMobile.forEach((stripe) => stripe.classList.toggle("touch-active"));
-    eventsSection.classList.toggle("translate-y-[100%]")
-});
+import BrowserRouter from "./components/BrowserRouter.js";
+import routes from "./routes.js";
 
-const burgerMenuDesktop = document.getElementById("burger-menu-desktop");
-const stripesDesktop = document.querySelectorAll("#event-toggler-desktop .stripe");
-burgerMenuDesktop.addEventListener("click", () => {
-    stripesDesktop.forEach((stripe) => stripe.classList.toggle("touch-active"));
-    eventsSection.classList.toggle("sm:translate-x-[-100%]")
-});
+BrowserRouter(document.getElementById("root"), routes);
+
 
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
   key: "AIzaSyCwRlYsm_3KSv8r8or-DLKZV8f3rDWdLpo",
