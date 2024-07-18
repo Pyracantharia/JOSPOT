0063c42b760e6439cf615dcf725729f8599fb725
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-07-19 00:15:18 +0200

    ajotu de ficher distant

--- html/js/map.js
+++ html/js/map.js
@@ -20,6 +20,7 @@
 });
 
 import SitesCompetition from "./views/SitesCompetition.js";
+import showNearestEventPopUp from "./components/Pop.js";
 
 let map;
 
@@ -99,34 +100,6 @@ export async function initMap() {
     await showNearestEventPopUp();
 }
 
-async function showNearestEventPopUp() {
-    const nearestEvent = await getNearestEvent();
-
-    const popUp = document.createElement("div");
-    popUp.classList.add("event-pop-up");
-    popUp.innerHTML = `
-        <h2>Événement à venir le plus proche</h2>
-        <p><strong>Lieu:</strong> ${nearestEvent.location}</p>
-        <p><strong>Date de début:</strong> ${new Date(nearestEvent.starting_date).toLocaleString()}</p>
-        <button id="close-pop-up">Fermer</button>
-    `;
-    document.body.appendChild(popUp);
-
-    document.getElementById("close-pop-up").addEventListener("click", () => {
-        document.body.removeChild(popUp);
-    });
-}
-
-async function getNearestEvent() {
-    const siteInfos = await SitesCompetition();
-    const currentDate = new Date();
-
-    // Filtrer les événements futurs et les trier par date
-    const futureEvents = siteInfos.filter(event => new Date(event.starting_date) >= currentDate);
-    futureEvents.sort((a, b) => new Date(a.starting_date) - new Date(b.starting_date));
-
-    return futureEvents[0]; // L'événement le plus proche
-}
 
 window.initMap = initMap;
 
