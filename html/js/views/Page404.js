4cd08f35d7aa9ec4e645daaafc2924533f9512d2
adam596 <adamgarchi@gmail.com>
2024-07-26 01:30:49 +0200

    Beutify 404 page

--- html/js/views/Page404.js
+++ html/js/views/Page404.js
@@ -4,22 +4,70 @@ class Page404 extends Component {
   render() {
     return {
       tag: 'div',
-      props: {
-        class: "w-screen h-screen relative".split(' ')
+      attributes: {
+        class: "w-screen h-screen flex items-center justify-center bg-gray-100".split(' ')
       },
       children: [
         {
           tag: 'div',
-          props: {
-            class: "absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]".split(' ')
+          attributes: {
+            class: "bg-white bg-opacity-70 p-12 rounded-lg shadow-lg text-center".split(' ')
           },
           children: [
             {
-              tag: 'h1',
-              props: {
-                class: "text-5xl text-slate-500".split(' ')
+              tag: 'img',
+              attributes: {
+                src: "img/jo.png",
+                class: "mx-auto mb-8 w-1/4 h-auto".split(' ')
+              }
+            },
+            {
+              tag: 'div',
+              attributes: {
+                class: "mb-8".split(' ')
               },
-              children: "ERROR 404"
+              children: [
+                {
+                  tag: "span",
+                  attributes: {
+                    class: "text-5xl text-gray-800 mb-12".split(" ")
+                  },
+                  children: [
+                    {
+                      tag: "span",
+                      attributes: {},
+                      children: "404"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              tag: 'div',
+              attributes: {
+                class: "text-4xl text-gray-800 mb-12".split(' ')
+              },
+              children: [
+                {
+                  tag: 'span',
+                  attributes: {},
+                  children: "Oops! The page you're looking for doesn't exist."
+                }
+              ]
+            },
+            {
+              tag: 'a',
+              attributes: {
+                class: "text-2xl text-blue-500 hover:underline".split(' '),
+                href: "/"
+              },
+              children: [
+                {
+                  tag: 'span',
+                  attributes: {},
+                  children: "Go back to Homepage"
+                }
+              ]
             }
           ]
         }
